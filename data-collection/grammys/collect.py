c433935902f176acb0becabfeb6812d25f22ecb0
Matthew Faw <matthewfaw@gmail.com>
2017-04-01 17:49:15 -0400

    Pretty print Entry objects

--- data-collection/grammys/collect.py
+++ data-collection/grammys/collect.py
@@ -12,6 +12,10 @@ class Entry:
         self.category = category
         self.nominee_work = nominee_work
         self.credits = credits
+    def __str__(self):
+        return "Year: %s\nCategory: %s\nNominee Work: %s\nCredits: %s" % (self.year,self.category,self.nominee_work,self.credits)
+    def __repr__(self):
+        return str(self)
 
 def get_all_grammy_pages():
     res = urllib2.urlopen(GRAMMY_DATA_URL)
