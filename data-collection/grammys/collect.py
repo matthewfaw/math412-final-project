0da23455858e7d61f878927462dfbe98fae10876
Matthew Faw <matthewfaw@gmail.com>
2017-04-04 15:03:15 -0400

    Adds documentation to functions

--- data-collection/grammys/collect.py
+++ data-collection/grammys/collect.py
@@ -18,6 +18,7 @@ class Entry:
         return str(self)
 
 def get_all_grammy_pages():
+    """Return a list of grammy pages to scrape.  Each page represents a different year"""
     res = urllib2.urlopen(GRAMMY_DATA_URL)
     html = res.read()
 
@@ -25,12 +26,19 @@ def get_all_grammy_pages():
     urls = [re.search('\".*\"',line).group(0).replace('"','') for line in lines]
     return urls
 
-def open_browser(url):
+def _open_browser(url):
+    """Opens Firefox browser, and loads the requested url"""
     driver = webdriver.Firefox()
     driver.get(url)
     return driver
 
-def scroll_to_bottom(driver, pauseTime):
+def _scroll_to_bottom(driver, pauseTime):
+    """
+    Given the driver controlling the currently opened web page,
+    scrolls to the bottom of the page
+
+    pauseTime is the amount of time the driver will wait between successive scroll events
+    """
     currHeight = driver.execute_script("return document.body.scrollHeight")
      
     while True:
@@ -42,11 +50,16 @@ def scroll_to_bottom(driver, pauseTime):
             break
         currHeight = nextHeight
 
-def get_grammy_field(entry, field):
+def _get_grammy_field(entry, field):
+    """Gets the grammy field from the requested table entry in the web page"""
     t =  entry.find_element_by_xpath(".//td[@class='views-field views-field-"+field+"']").text
     return t
 
-def construct_grammy_entries(entries):
+def _construct_grammy_entries(entries):
+    """
+    Given a list of Grammy entries in the HTML table,
+    construct the Grammy Entry objects, and return the objects as a list
+    """
     grammy_entries = []
     for i in range(0,len(entries)):
         entry = entries[i]
@@ -63,6 +76,7 @@ def construct_grammy_entries(entries):
 
 # https://paulhoganreid.wordpress.com/2015/01/19/using-python-and-selenium-to-scrape-an-infinitely-scrolling-table/
 def scrape_sections(url):
+    """Scrapes the requested url for grammy data. Returns a list of all grammy entries on the page"""
     driver = open_browser(url)
     scroll_to_bottom(driver, 3)
     table = driver.find_element_by_xpath("//table[@class='views-table cols-4']/tbody")
