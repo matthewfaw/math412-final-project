e48daf2040440c1263d4bc745423c763211d81c8
Michael Mendelsohn <MJMendelsohn@users.noreply.github.com>
2017-04-04 12:59:58 -0400

    Supports np Array Serialization

--- serializer/serializer.py
+++ serializer/serializer.py
@@ -1,11 +1,11 @@
-import json
+import pickle
 
 
 def serialize(data, string_id):
   with open('./data/'+ string_id + '.json', 'w') as open_file:
-    json.dump(data, open_file)
+    pickle.dump(data, open_file)
 
 
 def deserialize(string_id):
   with open('./data/' + string_id + '.json', 'r') as open_file:
-    return json.load(open_file)
+    return pickle.load(open_file)
