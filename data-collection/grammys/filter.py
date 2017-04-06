868c0716904b734b465470989783bd53c72b6ffb
Matthew Faw <matthewfaw@gmail.com>
2017-04-05 21:47:18 -0400

    Adds ability to get the artist name from the credits field

--- data-collection/grammys/filter.py
+++ data-collection/grammys/filter.py
@@ -1,11 +1,22 @@
+import re
 from csv_helper import deserialize, serialize
 
 def filter_by_category(array, category):
     return [e for e in array if e.category == category]
 
-x = deserialize('GRAMMIES.csv')
-xf = filter_by_category(x, 'Song Of The Year')
-serialize(xf,'FILTERED.csv')
+def _get_artist_name(credits):
+    search_items = [',', ' &', ' (']
+    artist_end = len(credits)
+    for item in search_items:
+        index = credits.find(item)
+        if index != -1:
+            artist_end = min(index, artist_end)
+    return credits[0:artist_end]
 
-e = xf[0]
-print e.year
+def convert_credits_to_names(array):
+    for entry in array:
+        entry.credits = _get_artist_name(entry.credits)
+    return array
+
+x = deserialize('20170405-213931NAMED.csv')
+print x[0]
