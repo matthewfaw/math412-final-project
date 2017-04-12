4969395aad81de57602d7af1ea688383561f23ed
Matthew Faw <matthewfaw@gmail.com>
2017-04-11 22:18:59 -0400

    Adds percentages to the not found

--- analyze_spotify_misses.py
+++ analyze_spotify_misses.py
@@ -15,7 +15,11 @@ not_found=deserialize('NOT_FOUND.csv')
 all_counts = generate_counts(all_grammies)
 not_found_counts = generate_counts(not_found)
 
-print 'CATEGORY: # not found / total #'
-for key in sorted(not_found_counts, key=not_found_counts.get):
-    print key,': ', not_found_counts[key], '/', all_counts[key]
+proportion_counts = {}
+for entry in all_counts:
+    proportion_counts[entry] = float(not_found_counts.get(entry,0))/float(all_counts[entry]) 
+
+print 'CATEGORY: proportion, # not found / total #'
+for key in sorted(proportion_counts, key=proportion_counts.get):
+    print key,': ', proportion_counts[key], ',', not_found_counts.get(key,0), '/', all_counts[key]
 
