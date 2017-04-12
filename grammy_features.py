cac5123fbf067ee04462b8616044cd7d2bb6d288
Matthew Faw <matthewfaw@gmail.com>
2017-04-11 23:45:13 -0400

    generate record of the yar

--- grammy_features.py
+++ grammy_features.py
@@ -5,7 +5,7 @@ from  serializer.serializer import serialize
 
 entries_list = []
 not_found_songs = []
-for entry in deserialize('NAMED.csv'):
+for entry in deserialize('RECORD_OF_THE_YEAR.csv'):
     feature = spotify_query(entry.nominee_work, entry.credits)
     if isinstance(feature, basestring):
         not_found_songs.append(entry)
@@ -14,7 +14,7 @@ for entry in deserialize('NAMED.csv'):
     entries_list.append(entry)
 
 csv_serialize(not_found_songs, 'NOT_FOUND.csv')
-csv_serialize(entries_list, "TEST.csv")
+csv_serialize(entries_list, "RECORD_OF_THE_YEAR.csv")
 for i in not_found_songs:
     print i
 print "Number of songs not found: ", len(not_found_songs)
