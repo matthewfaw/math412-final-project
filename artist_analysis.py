cd97be7e45198041e4576c6ed1eac9e90fe1a6e7
Michael Mendelsohn <MJMendelsohn@users.noreply.github.com>
2017-04-30 17:09:28 -0400

    Adds Basic Artist Analysis

---
+++ artist_analysis.py
@@ -0,0 +1,32 @@
+from serializer.serializer import deserialize as depickle
+import numpy as np
+import matplotlib.pyplot as plt
+import data_analysis.do_pca as pca
+import rips
+
+catalogs = depickle('artists_tracks_from_record_of_the_year_with_song_feature_vectors')
+
+def analyze_artist(artist_dict):
+    '''
+    Input:
+    a catalog, with populated feature vectors
+        { artist_name: { album_name: [[song_vector]] } }
+    Output:
+    persistence diagram for each of the artist's albums
+    '''
+    for album, song_vectors in artist_dict.iteritems():
+
+        vector_array = np.array(song_vectors)
+        cleaned_vectors = remove_null(vector_array)
+
+        pca_vector_array = pca.do_pca(cleaned_vectors)
+        bd_pairs = rips.one_tda(pca_vector_array, 0)
+        pca_bd_pairs = rips.one_tda(pca_vector_array, 0)
+
+        rips.plotDGM(pca_bd_pairs)
+        plt.show()
+
+def remove_null(vectors):
+    return np.array([list(row) for row in vectors if all(None is not x for x in row)])
+
+analyze_artist(catalogs['Billy Joel'])
